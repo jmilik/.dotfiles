d364f8575198434941f1c12cbcd9c20ddc2c9e41
Joseph Milik <jmilik@users.noreply.github.com>
2021-05-13 22:18:24 +0100

    update unmount script

--- .config/rofi/unmount.sh
+++ .config/rofi/unmount.sh
@@ -2,4 +2,4 @@
 
 mnt_location=$(find /mnt -mindepth 1 -maxdepth 1 -not -empty -type d | rofi -dmenu -p "unmount")
 [[ -z $mnt_location ]] && exit
-sudo -A umount "$mnt_location"
+sudo systemd-umount "$mnt_location"
